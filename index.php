39f520808a5bc74dfc35f0d961fd607f59847a14
Bintang Suharsono <bintang.mada.suharsono@bni.co.id>
2023-09-10 14:40:36 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -11,6 +11,10 @@
     <h1>Hello World</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus quasi commodi, ut architecto ratione officiis
         reiciendis nesciunt fuga. Nostrum, consequatur!</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 
 </html>
